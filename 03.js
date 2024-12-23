73045527c5ca071b6b6eb4f563228d21d9a0a5ae
Anshu kumar <kumaranshu303393@gmail.com>
2024-12-23 13:58:45 +0530

    Database Conversion Done

---
+++ 03.js
@@ -0,0 +1,34 @@
+let score = 52
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1
+// false = 0
+
+let isLoggedIn = 10
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1=> true; 0=> false
+// ""=> false
+// "Anshu"=> true
+// null => false
+// undefined => false
+// 15(number) => true
+
+let someNumber = true
+
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
